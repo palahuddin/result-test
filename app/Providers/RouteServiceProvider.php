c23e7fce6d9c4bf154a6db45e0660332489a9e7d
Alvin Tanjaya <me@alvintanjaya.com>
2021-03-22 11:15:50 +0700

    feat: add project skeleton
    Use laravel framework.

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->routes(
+            function () {
+                Route::namespace($this->namespace)
+                     ->group(base_path('routes/web.php'))
+                ;
+            }
+        );
+    }
+}
